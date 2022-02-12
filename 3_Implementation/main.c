d10002615ca5b3adb6292bdc3bbead2db65f44b5
Gowrishankar-Chandrasekar <98813206+1g1o0w1r1i@users.noreply.github.com>
2022-02-13 00:42:23 +0530

    Update main.c

--- 3_Implementation/main.c
+++ 3_Implementation/main.c
@@ -9,10 +9,10 @@ struct person
     char address[50];
      char father_name[35];
      char mother_name[30];
-    long int mobile_no;
+    long int mble_no;
     char sex[8];
     char mail[100];
-    char citizen_no[20];
+    char citision_no[20];
 
     };
 void menu();
@@ -40,12 +40,13 @@ void start()
 }
 void menu()
 {
+    system("cls");
 printf("\t\t**********WELCOME TO PHONEBOOK*************");
 
 printf("\n\n\t\t\t  MENU\t\t\n\n");
-printf("\t1.Add New   \t2.List   \t3.Exit  \n\t4.Modify \t5.Search \t6.Delete");
+printf("\t1.Add New   \t2.List   \t3.Exit  \n\t4.Modify \t5.Search\t6.Delete");
 
-switch (getch())
+switch(getch())
 {
     case '1':
 
@@ -66,6 +67,7 @@ switch (getch())
                 printf("\nEnter 1 to 6 only");
                 printf("\n Enter any key");
                 getch();
+
 menu();
 }
 }
@@ -82,7 +84,7 @@ menu();
         printf("\nEnter father name: ");
         got(p.father_name);
         printf("\nEnter mother name: ");
-        got(p.mother_name);m
+        got(p.mother_name);
         printf("\nEnter phone no.:");
         scanf("%ld",&p.mble_no);
         printf("Enter sex:");
@@ -92,11 +94,15 @@ menu();
         printf("\nEnter citizen no:");
         got(p.citision_no);
         fwrite(&p,sizeof(p),1,f);
-      flush (stdin);
+
+      fflush(stdin);
         printf("\nrecord saved");
+
 fclose(f);
+
     printf("\n\nEnter any key");
-	getch();
+
+     getch();
     system("cls");
     menu();
 }
@@ -108,6 +114,7 @@ f=fopen("project","rb");
 if(f==NULL)
 {
 printf("\nfile opening error in listing :");
+
 exit(1);
 }
 while(fread(&p,sizeof(p),1,f)==1)
@@ -115,8 +122,8 @@ while(fread(&p,sizeof(p),1,f)==1)
      printf("\n\n\n YOUR RECORD IS\n\n ");
         printf("\nName=%s\nAdress=%s\nFather name=%s\nMother name=%s\nMobile no=%ld\nSex=%s\nE-mail=%s\nCitizen no=%s",p.name,p.address,p.father_name,p.mother_name,p.mble_no,p.sex,p.mail,p.citision_no);
 
-	 getch();
-	 system("cls");
+     getch();
+     system("cls");
 }
 fclose(f);
  printf("\n Enter any key");
@@ -135,6 +142,7 @@ if(f==NULL)
 {
     printf("\n error in opening\a\a\a\a");
     exit(1);
+
 }
 printf("\nEnter name of person to search\n");
 got(name);
@@ -143,15 +151,16 @@ while(fread(&p,sizeof(p),1,f)==1)
     if(strcmp(p.name,name)==0)
     {
         printf("\n\tDetail Information About %s",name);
-        printf("\nName:%s\naddress:%s\nFather name:%s\nMother name:%s\nMobile no:%ld\nsex:%s\nE-mail:%s\nCitizen no:%s",p.name,p.address,p.father_name,p.mother_name,p.mble_no,p.sex,p.mail,p.citizen_no);
+        printf("\nName:%s\naddress:%s\nFather name:%s\nMother name:%s\nMobile no:%ld\nsex:%s\nE-mail:%s\nCitision no:%s",p.name,p.address,p.father_name,p.mother_name,p.mble_no,p.sex,p.mail,p.citision_no);
     }
         else
         printf("file not found");
+
 }
  fclose(f);
   printf("\n Enter any key");
 
-	 getch();
+     getch();
     system("cls");
 menu();
 }
@@ -159,74 +168,85 @@ void deleterecord()
 {
     struct person p;
     FILE *f,*ft;
-	int flag;
-	char name[100];
-	f=fopen("project","rb");
-	if(f==NULL)
-		{
-			printf("CONTACT'S DATA NOT ADDED YET.");
-		}
-	else
-	{
-		ft=fopen("temp","wb+");
-		if(ft==NULL)
-		{
-            printf("file opening error");
-		else
+    int flag;
+    char name[100];
+    f=fopen("project","rb");
+    if(f==NULL)
         {
-		printf("Enter CONTACT'S NAME:");
-		get(name);
 
-		fflush(stdin);
-		while(fread(&p,sizeof(p),1,f)==1)
-		{
-			if(strcmp(p.name,name)!=0)
-				fwrite(&p,sizeof(p),1,ft);
-			if(strcmp(p.name,name)==0)
+            printf("CONTACT'S DATA NOT ADDED YET.");
+
+        }
+    else
+    {
+        ft=fopen("temp","wb+");
+        if(ft==NULL)
+
+            printf("file opaning error");
+        else
+
+        {
+
+        printf("Enter CONTACT'S NAME:");
+        got(name);
+
+        fflush(stdin);
+        while(fread(&p,sizeof(p),1,f)==1)
+        {
+            if(strcmp(p.name,name)!=0)
+                fwrite(&p,sizeof(p),1,ft);
+            if(strcmp(p.name,name)==0)
                 flag=1;
-		}
-	fclose(f, ft);
-	if(flag!=1)
-	{
-		printf("NO CONTACT'S RECORD TO DELETE.");
-		remove("temp.txt");
-	}
-		else
-		{
-			remove("project");
-			rename("temp.txt","project");
-			printf("RECORD DELETED SUCCESSFULLY.");
-		}
-	}
+        }
+    fclose(f);
+    fclose(ft);
+    if(flag!=1)
+    {
+        printf("NO CONACT'S RECORD TO DELETE.");
+        remove("temp.txt");
+    }
+        else
+        {
+            remove("project");
+            rename("temp.txt","project");
+            printf("RECORD DELETED SUCCESSFULLY.");
+
+        }
+    }
 }
  printf("\n Enter any key");
 
-	 getch();
+     getch();
     system("cls");
 menu();
 }
+
 void modifyrecord()
 {
     int c;
     FILE *f;
     int flag=0;
     struct person p,s;
-	char  name[50];
-	f=fopen("project" "rb+");
-	if(f==NULL)
-		{
-			printf("CONTACT'S DATA NOT ADDED YET.");
-			exit(1);
-		}
-	else
-	{
-	    system("cls");
-		printf("\nEnter CONTACT'S NAME TO MODIFY:\n");
+    char  name[50];
+    f=fopen("project","rb+");
+    if(f==NULL)
+        {
+
+            printf("CONTACT'S DATA NOT ADDED YET.");
+            exit(1);
+
+        }
+    else
+    {
+        system("cls");
+        printf("\nEnter CONTACT'S NAME TO MODIFY:\n");
             got(name);
-            while(fread(&p,sizeof(p),1,f)=1)
+            while(fread(&p,sizeof(p),1,f)==1)
             {
                 if(strcmp(name,p.name)==0)
-		{
+                {
+
+
                     printf("\n Enter name:");
                     got(s.name);
                     printf("\nEnter the address:");
@@ -236,37 +256,44 @@ void modifyrecord()
                     printf("\nEnter mother name:");
                     got(s.mother_name);
                     printf("\nEnter phone no:");
-                    scanf("%ld",&s.mobile_no);
+                    scanf("%ld",&s.mble_no);
                     printf("\nEnter sex:");
                     got(s.sex);
                     printf("\nEnter e-mail:");
                     got(s.mail);
                     printf("\nEnter citizen no\n");
-                    got(s.citizen_no);
+                    got(s.citision_no);
                     fseek(f,-sizeof(p),SEEK_CUR);
                     fwrite(&s,sizeof(p),1,f);
                     flag=1;
                     break;
+
+
                 }
                 fflush(stdin);
+
             }
             if(flag==1)
             {
                 printf("\n your data id modified");
-	    }
+
+            }
             else
             {
                     printf(" \n data is not found");
+
             }
             fclose(f);
-	}
-	 printf("\n Enter any key");
-	 getch();
+    }
+     printf("\n Enter any key");
+     getch();
     system("cls");
-	menu();
+    menu();
+
 }
 void got(char *name)
 {
+
    int i=0,j;
     char c,ch;
     do
@@ -290,8 +317,13 @@ void got(char *name)
                     {
                         ch=*(name+j);
                         putch(ch);
+
                     }
+
                 }
     }while(c!=13);
       *(name+i)='\0';
 }
+
+
+
